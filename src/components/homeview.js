8e20dd8e991c5f04298ccafb0b2697cc98838802
abhinavsri000 <abhinavsri000@gmail.com>
2020-04-24 13:33:26 +0530

    new_component

---
+++ src/components/homeview.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+class HomeView extends Component {
+    render() {
+        return (  <Text>
+            Sample Text
+        </Text>);
+    }
+}export default HomeView
